d015e125b5b124ffa2c5b3300f95508d4a4d0066
jsp <zinsani@gmail.com>
2025-05-10 18:10:23 +0900

    Fix font face not being applied properly with semi-bold and bold

--- src/app/providers/theme.tsx
+++ src/app/providers/theme.tsx
@@ -4,10 +4,9 @@ import type { PropsWithChildren } from "react";
 function Theme({ children }: PropsWithChildren) {
   const config: ThemeConfig = {
     token: {
-      fontFamily: `Pretendard-Regular, Pretendard-SemiBold, Pretendard-Bold, 
--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 
-Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 
-'Segoe UI Symbol', 'Noto Color Emoji'`,
+      fontFamily: `Pretendard, -apple-system, BlinkMacSystemFont, 
+'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 
+'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
     },
     components: {
       Table: {
