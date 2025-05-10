69948835839a42769e0413bd89dc97d960c72013
jsp <zinsani@gmail.com>
2025-05-10 15:21:33 +0900

    Add font-face for Pretendard-SemiBold and Bold

--- src/app/providers/theme.tsx
+++ src/app/providers/theme.tsx
@@ -4,9 +4,16 @@ import type { PropsWithChildren } from "react";
 function Theme({ children }: PropsWithChildren) {
   const config: ThemeConfig = {
     token: {
-      fontFamily: `Pretendard-Regular, -apple-system, BlinkMacSystemFont, 
-'Segoe UI', Roboto, 'Helvetica Neue', Arial, 'Noto Sans', sans-serif, 
-'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'`,
+      fontFamily: `Pretendard-Regular, Pretendard-SemiBold, Pretendard-Bold, 
+-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', 
+Arial, 'Noto Sans', sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 
+'Segoe UI Symbol', 'Noto Color Emoji'`,
+    },
+    components: {
+      Table: {
+        cellPaddingBlock: 7.5,
+        cellPaddingInline: 7.5,
+      },
     },
   };
   return <ConfigProvider theme={config}>{children}</ConfigProvider>;
